b4f5136908e948170b3d94dd71cec8ca40df8199
mabin <mabin20231996@gmail.com>
2025-02-18 00:26:11 +0800

    <learn><while>

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-print("hello,world")
+# print("hello,world")
+n = 100
+
+sum = 0
+counter = 1
+while counter <= n:
+    sum = sum + counter
+    counter += 1
+
+print("1 到 %d 之和为： %d" % (n, sum))
+
+# 无限循环
+# var = 1
+# while var == 1:
+#     num = int(input("输入一个数字："))
+#     print("你输入的数字是：", num)
+
+count = 0
+while count < 5:
+    print(count, "小于5")
+    count += 1
+else:
+    print(count, "大于或等于5")
